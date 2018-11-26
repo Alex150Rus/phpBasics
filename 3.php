55a18fe192c62ac0bc93b549af386eed16f659f6
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-26 14:46:35 +0300

    added all php files

---
+++ 3.php
@@ -0,0 +1,23 @@
+﻿<?php
+$h1 = '<h1>Привет<h1>';
+$title = '<title>Привет</title>';
+$date = date("d.m.y") . "- текущая дата<br>";
+$time = date("h:i:s") . "- текущее время";
+$html = <<<php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    $title
+</head>
+<body>
+    $h1
+    $date
+    $time
+</body>
+</html>
+php;
+
+echo $html;
---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Простейший PHP-сценарий</title>
+</head>
+<body>
+<h1>Здравствуйте</h1>
+<p>
+<?php
+//вычисляем текущую дату в формате "день.месяц.год"
+$dat = date("d.m y");
+
+//вычисляем текущее время
+$tm = date ("h:i:s");
+
+# выводим их
+echo "Текущая дата: $dat года<br />\n";
+echo "Текущее время: $tm<br />\n";
+
+# выводим цифры
+echo "А вот квадраты и кубы первых пяти натуральных чисел:<br />\n";
+echo "<ul>\n";
+for ($i = 1; $i <=5; $i++) {
+    echo "<li>$i в квадрате = " . ($i * $i);
+    echo ", $i в кубе = " . ($i * $i *$i) . "</li>\n";
+}
+?>
+</ul>
+</p>
+<?php 
+echo PHP_INT_MAX;
+$a = 1;
+$b = 2;
+echo "<br>$a, $b";
+
+$b = $a++;
+
+echo "<br>$a, $b";
+?>
+    
+</body>
+</html>
